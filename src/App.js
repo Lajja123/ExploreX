412ba7d5f2e64012568793b59d4dd4f6fcc46b0d
Lajja123 <lajja.lamprostech@gmail.com>
2023-10-04 22:17:53 +0530

    add favicon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-import { Widget, GetHash } from "explorex";
 import Home from "./components/Home";
+
 
 function App() {
   return (
     <div className="App">
       <Home />
+  
     </div>
   );
 }
