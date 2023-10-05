5a3d83b665720797708c003c3ccde95c129d2adb
Lajja123 <lajja.lamprostech@gmail.com>
2023-10-05 12:14:55 +0530

    add mobile responsive style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Home from "./components/Home";
-import { Widget } from "explorex";
 
 function App() {
   return (
     <div className="App">
       <Home />
-      {/* <Widget address="TLp9FwsZEc3MaKQVd8Ls1r2KoVKimRH9HA" /> */}
     </div>
   );
 }
